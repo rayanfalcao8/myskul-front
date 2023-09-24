5557c97bb486db0e161f6281ab834c68d58701d7
Blaise Fotso <blaise.fotso@2tcorp.org>
2023-09-24 18:12:57 +0100

    Show onBoarding

---
+++ lib/on_boarding/subscription_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class SubscriptionScreen extends StatelessWidget {
+  const SubscriptionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
