b521b21865877a4d5bce4b16f8ea8776afdaa331
Blaise Fotso <blaise.fotso@2tcorp.org>
2023-10-16 06:40:26 +0100

    Hide categories on shop list

--- lib/screens/shop/product_list_page.dart
+++ lib/screens/shop/product_list_page.dart
@@ -65,17 +65,17 @@ class _ProductListPageState extends State<ProductListPage> {
               const SizedBox(
                 height: 20,
               ),
-              Container(
-                height: 40,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: List.generate(categories.length,
-                      (index) => _buildCatBadge(categories[index])),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
+              // Container(
+              //   height: 40,
+              //   child: ListView(
+              //     scrollDirection: Axis.horizontal,
+              //     children: List.generate(categories.length,
+              //         (index) => _buildCatBadge(categories[index])),
+              //   ),
+              // ),
+              // const SizedBox(
+              //   height: 20,
+              // ),
               if (!_products.isEmpty)
                 Expanded(
                   child: GridView.count(
