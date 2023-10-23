3e059d917625498aa29433cc4b94280a0c449a10
Blaise Fotso <blaise.fotso@2tcorp.org>
2023-10-23 19:12:15 +0100

    Update on_boarding

--- lib/introduction_screen.dart
+++ lib/introduction_screen.dart
@@ -6,60 +6,48 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class IntroScreen extends StatelessWidget {
   IntroScreen({super.key});
-  final List<PageViewModel> _pages = [
-    PageViewModel(
-      title: "Bienvenue sur MySkull",
-      body:
-          "Votre plateforme de e-learning vous permettant d'étudier avec plusieurs options possibles",
-      image: Container(
-        child: Image.asset(
-          "assets/images/learning.png",
-          fit: BoxFit.fitHeight,
-        ),
-      ),
-      decoration: getPageDec(),
-    ),
-    PageViewModel(
-      title: "Souscription",
-      body:
-          "Pour profiter des différents cours offerts par l'application il vous faut souscrire à un abonnement",
-      image: Container(
-        child: Image.asset(
-          "assets/images/learning.png",
-          fit: BoxFit.fitHeight,
-        ),
-      ),
-      decoration: getPageDec(),
-    ),
-    PageViewModel(
-      title: "Quiz",
-      body:
-          "Révisez de manière interractive et divertissant avec une panoplie de questions organisées sous forme de Quiz !",
-      image: Center(
-        child: Image.asset("assets/images/learning.png"),
-      ),
-      decoration: getPageDec(),
-    )
-  ];
+  List<Widget> _pages = [];
 
   @override
   Widget build(BuildContext context) {
+    _pages = [
+      _buildPage(
+        "Bienvenue sur MySkull",
+        "Votre plateforme de e-learning vous permettant d'étudier avec plusieurs options possibles",
+        "assets/images/learning.png",
+      ),
+      _buildPage(
+        "Souscription",
+        "Souscrivez à un abonnement pour profiter de multiples avantages tel que des cours et des quiz offerts par l'application !",
+        "assets/images/subscription.png",
+      ),
+      _buildPage(
+        "Quiz",
+        "Révisez de manière interractive et divertissante avec une panoplie de questions organisées sous forme de Quiz !",
+        "assets/images/quiz.png",
+      )
+    ];
+
     return Scaffold(
       appBar: getAppBar(
           title: "Introduction", context: context, showBackBtn: false),
-      body: IntroductionScreen(
-        pages: _pages,
-        dotsDecorator: const DotsDecorator(
-          size: Size(10, 10),
-          activeSize: Size.square(15),
-          activeColor: Color(0xff22987f),
+      body: Container(
+        decoration: getBckDecoration(),
+        child: IntroductionScreen(
+          rawPages: _pages,
+          dotsDecorator: const DotsDecorator(
+            size: Size(10, 10),
+            activeSize: Size.square(15),
+            activeColor: Color(0xff22987f),
+          ),
+          showDoneButton: true,
+          globalBackgroundColor: Colors.transparent,
+          done: const Text("Terminé", style: TextStyle(fontSize: 15)),
+          showSkipButton: true,
+          skip: const Text("Passer", style: TextStyle(fontSize: 15)),
+          next: const Icon(Icons.arrow_forward, size: 20),
+          onDone: () => _onDone(context),
         ),
-        showDoneButton: true,
-        done: const Text("Terminé", style: TextStyle(fontSize: 15)),
-        showSkipButton: true,
-        skip: const Text("Passer", style: TextStyle(fontSize: 15)),
-        next: const Icon(Icons.arrow_forward, size: 20),
-        onDone: () => _onDone(context),
       ),
     );
   }
@@ -69,5 +57,38 @@ class IntroScreen extends StatelessWidget {
     await prefs.setBool("ON_BOARDING", false);
     Navigator.pushReplacement(
         context, MaterialPageRoute(builder: (context) => Login()));
+  }
+
+  Widget _buildPage(String title, String body, String imagePath) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(height: 60),
+        Container(
+          height: 320,
+          child: Image.asset(
+            imagePath,
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+        SizedBox(height: 30),
+        Text(
+          title,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Color(0xff22987f),
+            fontSize: 27,
+          ),
+        ),
+        SizedBox(height: 20),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Text(
+            body,
+            style: TextStyle(fontSize: 16),
+          ),
+        )
+      ],
+    );
   }
 }
